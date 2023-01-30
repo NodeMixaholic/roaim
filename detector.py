1d4a603fa7473d20551892707107a3dc8f2cfd66
NodeMixaholic <sam@sllord.info>
2023-01-30 11:23:47 -0600

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,29 +1,22 @@
+import torch
 import cv2
 import numpy as np
-from mss import mss
-import cv2.dnn as dnn
+from PIL import Image
+import torchvision.transforms as T
 
-def detect(net, image):
-    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-    net.setInput(blob, scale=1.0/255)
-    layer_outputs = net.forward(net.getUnconnectedOutLayersNames())
-    
-    boxes, confidences, class_ids = [], [], []
-    for output in layer_outputs:
-        for detection in output:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            
-            if confidence > 0.5:
-                box = detection[0:4] * np.array([image.shape[1], image.shape[0], image.shape[1], image.shape[0]])
-                (center_x, center_y, width, height) = box.astype("int")
-                
-                x = int(center_x - (width / 2))
-                y = int(center_y - (height / 2))
-                
-                boxes.append([x, y, int(width), int(height)])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-                
-    return boxes, confidences, class_ids
+
+def detect(model, image):
+    transform = T.Compose([
+        T.Resize(640),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    image_tensor = transform(image).unsqueeze(0)
+    output = model(image_tensor)
+
+    boxes = output[0]['boxes'].detach().numpy()
+    scores = output[0]['scores'].detach().numpy()
+    labels = output[0]['labels'].detach().numpy()
+
+    return boxes, scores, labels
