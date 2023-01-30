bc23304bcafa1e0a9f8965b3a65da9733933fef1
NodeMixaholic <sam@sllord.info>
2023-01-30 09:52:59 -0600

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,29 +1,20 @@
-import torch
-import torchvision.transforms as transforms
-from PIL import Image
-
-def detect(net, window_screenshot):
-    # Convert the screenshot to PIL Image and resize it
-    window_screenshot = Image.fromarray(window_screenshot)
-    window_screenshot = window_screenshot.resize((416, 416))
-    
-    # Transform the image for input to the network
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-    window_screenshot = transform(window_screenshot).unsqueeze(0)
-
+def detect(net, image):
+    # Convert numpy array to PyTorch tensor
+    image = torch.from_numpy(image)
+    # Add batch dimension
+    image = image.unsqueeze(0)
+    # Normalize the image
+    mean = [0.5, 0.5, 0.5]
+    std = [0.5, 0.5, 0.5]
+    image = (image / 255.0 - mean) / std
     # Pass the image through the network
     with torch.no_grad():
-        output = net(window_screenshot)
-    
+        output = net(image)
     # Extract the bounding boxes and confidence scores from the output
-    boxes = output[0][:, :4].detach().cpu().numpy()
-    confidences = output[0][:, 4].detach().cpu().numpy()
+    boxes = output[0][:, :4].detach().numpy()
+    confidences = output[0][:, 4].detach().numpy()
     # Filter detections with low confidence
     mask = confidences > 0.5
     boxes = boxes[mask]
     confidences = confidences[mask]
-    
     return boxes, confidences
