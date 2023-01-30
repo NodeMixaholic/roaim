069252bee4cc44e5a4008efed908fc1338f12549
NodeMixaholic <sam@sllord.info>
2023-01-30 08:56:39 -0600

    Update aimbot.py

--- aimbot.py
+++ aimbot.py
@@ -9,7 +9,7 @@ from hubconf import custom
 import pyautogui
 import time
 import win32api, win32con, win32gui
-model = custom(path_or_model="!best.pt")
+net = custom(path_or_model="!best.pt")
 size_scale = 3
 
 with mss.mss() as sct:
@@ -19,41 +19,35 @@ with mss.mss() as sct:
         hwnd = win32gui.FindWindow(None, 'Roblox')
         rect = win32gui.GetWindowRect(hwnd)
         region = rect[0], rect[1], rect[2] - rect[0], rect[3] - rect[1]
-        # Get image of screen
-        ori_img = np.array(pyautogui.screenshot(region=region))
-        ori_img = cv2.resize(ori_img, (ori_img.shape[1] // size_scale, ori_img.shape[0] // size_scale))
-        image = np.expand_dims(ori_img, 0)
-        img_w, img_h = image.shape[2], image.shape[1]
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)             
-        results = model(img)
-        results.render()
-        results = results.xyxyn
-        out = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        cv2.imshow('s', out)
-        
+        wx,wy,ww,wh = region[0], region[1], region[2], region[3]
+        # Capture a window screenshot
+        window_screenshot = np.array(pyautogui.screenshot(region=region))
+        # Detect objects in the screenshot
+        predictions = detect(net, window_screenshot)
 
-        # Pixel difference between crosshair(center) and the closest object
-        labels, cords = results[0][:, -1].cpu().numpy(), results[0][:, :-1].cpu().numpy()
-        try:
-            coords = coords.split(", ").split(",")
-        except:
-            print("coords is not a string")
-        try:
-            x = ((img_h/img_w)*1000) - (cords[0] - img_w)
-            y = ((img_h/img_w)*1000) - img_h/2 - (cords[1] * (img_h)) * 0.45
+        # Extract positions and classes of detected objects
+        objects = []
+        for pred in predictions:
+            x, y, w, h = pred[2]
+            objects.append((x + w / 2, y + h / 2, pred[0]))
 
-            # Move mouse and shoot
-            scale = 1.7 * size_scale
-            x = int(x * scale)
-            y = int(y * scale)
-            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x, y, 0, 0)
-            time.sleep(0.05)
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-            time.sleep(0.1)
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-        except:
-            print('not ready')
-        print('fps: {}'.format(1 / (time.time() - t)))
+        # Find the closest object
+        mouse_position = pywin32.GetCursorPos()
+        closest_object = None
+        min_distance = float("inf")
+        for obj in objects:
+            distance = np.sqrt((mouse_position[0] - obj[0]) ** 2 + (mouse_position[1] - obj[1]) ** 2)
+            if distance < min_distance:
+                closest_object = obj
+                min_distance = distance
+
+        # Calculate the change in the mouse position
+        dx = int(closest_object[0] - ww / 2)
+        dy = int(closest_object[1] - wh / 2)
+
+        # Move the window and mouse cursor by the calculated amount
+        pyautogui.moveRel(dx, dy)
+        pywin32.SetCursorPos((mouse_x + dx, mouse_y + dy))
 
         if cv2.waitKey(1) == 27:
             break
