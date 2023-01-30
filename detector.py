464146e486ee30b9462b95f889915fa91d226cdb
NodeMixaholic <sam@sllord.info>
2023-01-30 09:31:11 -0600

    Update detector.py

--- detector.py
+++ detector.py
@@ -8,7 +8,7 @@ def detect(net, image):
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
-    image = transform(image).unsqueeze(0)
+    image = transform(image)
     
     # Pass the image through the network
     with torch.no_grad():
