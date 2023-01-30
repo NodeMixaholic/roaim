72c77a824570ef8bd3b39c0d4ae2862ff95f21a6
NodeMixaholic <sam@sllord.info>
2023-01-30 09:56:39 -0600

    Update detector.py

--- detector.py
+++ detector.py
@@ -2,23 +2,33 @@ import torch
 import torchvision.transforms as transforms
 from PIL import Image
 
-def detect(net, image):
-    # Convert numpy array to PyTorch tensor
-    image = torch.from_numpy(image)
-    # Add batch dimension
-    image = image.unsqueeze(0)
-    # Normalize the image
-    mean = [0.5, 0.5, 0.5]
-    std = [0.5, 0.5, 0.5]
-    image = (image / 255.0 - mean) / std
+def detect(net, window_screenshot):
+    # Convert the screenshot to PIL Image and resize it
+    window_screenshot = Image.fromarray(window_screenshot)
+    window_screenshot = window_screenshot.resize((416, 416))
+    
+    # Transform the image for input to the network
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    window_screenshot = transform(window_screenshot).unsqueeze(0)
+
     # Pass the image through the network
     with torch.no_grad():
-        output = net(image)
-    # Extract the bounding boxes and confidence scores from the output
-    boxes = output[0][:, :4].detach().numpy()
-    confidences = output[0][:, 4].detach().numpy()
-    # Filter detections with low confidence
-    mask = confidences > 0.5
-    boxes = boxes[mask]
-    confidences = confidences[mask]
+        output = net(window_screenshot)
+    
+    # Extract the bounding boxes, confidences, and class_ids from the output
+    boxes, confidences, class_ids = [], [], []
+    for detection in output:
+        scores, class_idx = torch.max(detection[:, 5:], dim=1)
+        confidence = scores.flatten()
+        boxes = detection[:, :4]
+        
+        # Filter detections with low confidence
+        valid_detections = confidence > 0.5
+        boxes = boxes[valid_detections]
+        confidences = confidence[valid_detections]
+        class_ids = class_idx[valid_detections]
+    
     return boxes, confidences
