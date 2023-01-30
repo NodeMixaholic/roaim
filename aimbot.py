cd0c1a20e61b3fe1ed198a4dd6d682e9d5c3cebb
NodeMixaholic <sam@sllord.info>
2023-01-30 10:50:48 -0600

    Update aimbot.py

--- aimbot.py
+++ aimbot.py
@@ -24,8 +24,7 @@ with mss() as sct:
         region = rect[0], rect[1], rect[2] - rect[0], rect[3] - rect[1]
         wx,wy,ww,wh = region[0], region[1], region[2], region[3]
         # Capture a window screenshot
-        window_screenshot = cv2.cvtColor(np.array(ImageGrab.grab(bbox=(0, 0, ww, wh))), cv2.COLOR_BGR2RGB)
-
+        window_screenshot = cv2.cvtColor(np.array(sct.grab({"top": wy, "left": wx, "width": ww, "height": wh})), cv2.COLOR_BGR2RGB)
         # Detect objects in the screenshot
         predictions = detect(net, window_screenshot)
 
@@ -36,25 +35,25 @@ with mss() as sct:
             objects.append((x + w / 2, y + h / 2, pred[0]))
 
         # Find the closest object
-        mouse_position = pywin32.GetCursorPos()
+        mouse_x, mouse_y = win32api.GetCursorPos()
         closest_object = None
         min_distance = float("inf")
         for obj in objects:
-            distance = np.sqrt((mouse_position[0] - obj[0]) ** 2 + (mouse_position[1] - obj[1]) ** 2)
+            distance = np.sqrt((mouse_x - obj[0]) ** 2 + (mouse_y - obj[1]) ** 2)
             if distance < min_distance:
                 closest_object = obj
                 min_distance = distance
 
         # Calculate the change in the mouse position
-        dx = int(closest_object[0] - window_width / 2)
-        dy = int(closest_object[1] - window_height / 2)
+        dx = int(closest_object[0] - ww / 2)
+        dy = int(closest_object[1] - wh / 2)
 
         # Move the window and mouse cursor by the calculated amount
         pyautogui.moveRel(dx, dy)
-        pywin32.SetCursorPos((mouse_x + dx, mouse_y + dy))
+        win32api.SetCursorPos((mouse_x + dx, mouse_y + dy))
 
         if cv2.waitKey(1) == 27:
-            break
+                break
         time.sleep(0.01)
 
 cv2.destroyAllWindows()
